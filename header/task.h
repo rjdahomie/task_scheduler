ae8be1517684f49b04fe7829d56d7015fee48653
rjdahomie <rasajahromi@gmail.com>
2022-05-11 11:43:54 -0700

    Added the task constructor

--- header/task.h
+++ header/task.h
@@ -15,7 +15,7 @@ class task {
 	
 
     public:
-	Task(int task, string task title);
+	Task(int task, string title);
 	Task(int taskID, string title, string description, string classification, string priority, Time duration, Date dueDate, string location, list<Assignee> assigneeList);
 	void setID(int);
 	int getID();
