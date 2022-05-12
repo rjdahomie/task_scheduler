362cc76ebcbfe713e0de38492f102771f1735682
Shipapa1 <myduyisepic@gmail.com>
2022-05-11 18:06:47 -0700

    BeginningTimecppFile

---
+++ src/time.cpp
@@ -0,0 +1,31 @@
+#include "../header/time.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Time::Time(){
+}
+
+Time::Time(int inputDay, int inputHour, int inputMinute){
+}
+
+void Time::setDay(int){
+}
+
+void Time::setHour(int){
+}
+
+void Time::setMinute(int){
+}
+
+int Time::getDay(){
+}
+
+int Time::getHour(){
+}
+
+int Time::getMinute(){
+}
+
+
+
