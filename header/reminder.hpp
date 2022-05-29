0dff832286066bbcb6909c6792dbc031d8f3400d
thecornerhob <alex.rabinovich4050@gmail.com>
2022-05-28 17:28:04 -0700

    Updated reminder class files

--- header/reminder.hpp
+++ header/reminder.hpp
@@ -1,35 +1,30 @@
 #ifndef REMINDER_HPP
 #define REMINDER_HPP
 
-#include "task.h"
-#include "date.h"
+#include "../header/task.h"
 #include <string>
 
 using namespace std;
 
 class Reminder : public Task{
 	private:
-		int reminderID;
 		bool active;
 		int remind;
 		string reminderTitle;
 	public:
 		Reminder();
-		Reminder(int newReminderID, string newTitle, int newRemind, bool status);
+		Reminder(string newTitle, int newRemind, bool status);
 		Reminder(Task T);
 		~Reminder();
-		void setReminderID(int);
 		void setTitle(string);
 		void setStatus(bool);
 		void setDay(int);
-		int getReminderID();
 		string getTitle();
 		bool getStatus();
-		int getDays();
+		int getDay();
 		void addReminder();
 		void editReminder();
 		void deleteReminder();
-		void checkRemind(Date curr, Date due);
 		void displayReminder();
 };
 
--- src/reminder.cpp
+++ src/reminder.cpp
@@ -1,40 +1,32 @@
 #include "../header/reminder.hpp"
 #include <string>
 #include "../header/task.h"
-#include "../header/date.h"
+#include "../header/time.h"
 #include <iostream>
 
 using namespace std;
 
 Reminder::Reminder(){
-	reminderID = 0;
 	reminderTitle = "";
 	active = false;
-	remind = 0;
+	remind = 0;;
 }
 
-Reminder::Reminder(int newID, string remindTitle, int time, bool activity){
-	reminderID = newID;
+Reminder::Reminder(string remindTitle, int remindTime, bool activity){
 	reminderTitle = remindTitle;
-	remind = time;
+	remind = remindTime;
 	active = activity;
 }
 
-Reminder::Reminder(Task T){
-	reminderID = T.getID();
+Reminder::Reminder(Task T, int length){
 	reminderTitle = T.getTitle();
-	remind = T.getDate();
+	remind = length;
 	active = true;
 
 Reminder::~Reminder(){
-	delete reminderID;
 	delete reminderTitle;
 	delete active;
 	delete remind;
-}
-
-void Reminder::setReminderID(int setID){
-	reminderID = setID;
 }
 
 void Reminder::setTitle(string newTitle){
@@ -49,10 +41,6 @@ void Reminder::setDay(int newTime){
 	remind = newTime;
 }
 
-int Reminder::getReminderID(){
-	return reminderID;
-}
-
 string Reminder::getTitle(){
 	return reminderTitle;
 }
@@ -61,33 +49,29 @@ bool Reminder::getStatus(){
 	return active;
 }
 
-int Reminder::getDays(){
+int Reminder::getDay(){
 	return remind;
 }
 
-void Reminder::addReminder(int newID, int time, bool activity){
-	new Reminder(newID, time, activity);
+void Reminder::addReminder(string title, int time, bool activity){
+	new Reminder(title, time, activity);
 }
 
 void Reminder::editReminder(){
 	int choice = 0;
-	int oneAndThree;
+	int time;
 	string input;
-	cout << "1: Change ID" << endl;
-	cout << "2: Change Title" << endl;
-	cout << "3: Change Time(in days)" << endl;
-	cout << "4: Change Status" << endl;
+	cout << "1: Change Title" << endl;
+	cout << "2: Change Time(in days)" << endl;
+	cout << "3: Change Status" << endl;
 	cin >> choice;
 	if(choice = 1){
-		cin >> oneAndThree;
-		reminderID = oneAndThree;
-	}else if(choice = 2){
 		cin >> input;
 		reminderTitle = input;
+	}else if(choice = 2){
+		cin >> time;
+		remind = time;
 	}else if(choice = 3){
-		cin >> oneAndThree;
-		remind = oneAndThree;
-	}else if(choice = 4){
 		cout << "True or False(input F or T)" << endl;
 		cin >> input;
 		if(input = "F" || input = "f"){
@@ -100,10 +84,6 @@ void Reminder::editReminder(){
 
 void Reminder::deleteReminder(Reminder R){
 	delete R;
-}
-
-void Reminder::checkRemind(Date curr, Date due){
-	cout << "Days left till due: " << due.convertDate() - curr.convertDate() << endl;
 }
 
 void Reminder::displayReminder(){
